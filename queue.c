016e66c3c332c2074892bf7d0e4b0b7c15dac4c8
Eric Lai <0955.lai@gmail.com>
2023-02-28 01:50:03 +0800

    Add some special case condition for q_reverse

--- queue.c
+++ queue.c
@@ -185,6 +185,9 @@ void q_swap(struct list_head *head)
 /* Reverse elements in queue */
 void q_reverse(struct list_head *head)
 {
+    if (!head || list_empty(head))
+        return;
+
     struct list_head *node, *safe;
     list_for_each_safe (node, safe, head) {
         node->next = node->prev;
